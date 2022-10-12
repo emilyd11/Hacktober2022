aa8b0c74452777f4244a1b0c7838551ff3543490
Emily <63865165+emilyd11@users.noreply.github.com>
2022-10-12 17:27:03 -0400

    added some beginning scripts

---
+++ Hacktober2022/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    public  GameObject factory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
